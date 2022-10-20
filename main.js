db81f0ac887824344eb0f0c0dd9855fe04e1aa9d
MR-C0DE <80490263+MR-C0DE@users.noreply.github.com>
2022-10-20 15:45:36 -0400

    nouvelle version 0.0.2

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 let paragraphe = document.querySelector('p');
+let input = document.querySelector('input');
+let bouton = document.querySelector('button');
+
+
+bouton.addEventListener('click', function (e) { 
+    paragraphe.textContent = input.value;
+ });
